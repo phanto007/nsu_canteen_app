f78bdf63bcd64d19cf6975244ccfbd1af02cf07b
phanto007 <48092034+phanto007@users.noreply.github.com>
2019-03-12 20:42:12 +0600

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 $servername = "localhost";
-$server_user = "root";
-$server_pass = "";
-$dbname = "table3";
+$server_user = "crypkbij_cryptomayday";
+$server_pass = "crypkbij_cryptomayday";
+$dbname = "crypkbij_cryptomayday";
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
 $con = new mysqli($servername, $server_user, $server_pass, $dbname);
