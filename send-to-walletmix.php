be77ac47496e8a74c72fc8da7ffbbae8e8617029
phanto007 <48092034+phanto007@users.noreply.github.com>
2019-03-12 20:38:29 +0600

    Update send-to-walletmix.php

--- send-to-walletmix.php
+++ send-to-walletmix.php
@@ -74,9 +74,9 @@ if($_SESSION['customer_sid']==session_id())
     
     $walletmix->set_merchant_order_id($order_id);
      
-    $walletmix->set_app_name('nsu.ddns.net');
+    $walletmix->set_app_name('cryptomayday.com');
     $walletmix->set_currency('BDT');
-    $walletmix->set_callback_url('http://nsu.ddns.net/table3/data_callback.php');
+    $walletmix->set_callback_url('http://http://cryptomayday.com/data_callback.php');
     
     $extra_data = array();
      
