d2740b275ce5319086816d296337c3ad1d3377da
dipanzan <dipanzan@github.com>
2019-03-17 20:34:09 +0600

    Modified QR Scanner

--- qr-scanner.php
+++ qr-scanner.php
@@ -36,11 +36,15 @@
 
       Instascan.Camera.getCameras().then(function (cameras) {
 
-        if (cameras.length > 0) {
+        if (cameras.length == 1) {
 
-          scanner.start(cameras[1]);
+          scanner.start(cameras[0]);
 
-        } else {
+        } 
+        else if (cameras.length > 1) {
+            scanner.start(cameras[1]);
+        }
+        else {
 
           console.error('No cameras found.');
 
