4ca992fd092cf06c70542cfdd448d03f8f6f7ad5
dipanzan <dipanzan@github.com>
2019-03-17 16:54:12 +0600

    Updated verify-order.php

--- verify-order.php
+++ verify-order.php
@@ -3,9 +3,11 @@ include 'includes/connect.php';
 include 'includes/wallet.php';
 include 'includes/functions.php';
 
-// && isset($_POST['o_id'] && isset($_POST['e_str']))
-if($_SESSION['customer_sid'] == session_id()) {
+if($_SESSION['customer_sid'] == session_id() && isset($_GET['o_id']) && isset($_GET['e_str'])) {
 	
+	// Encrypted string
+	$e_str = $_GET['e_str'];
+
 	// Get user pkey
 	$pkey = "";
 
@@ -15,48 +17,27 @@ if($_SESSION['customer_sid'] == session_id()) {
 		$pkey =  $row['pkey'];
 	}
 
-	echo 'pkey: '.$pkey;
-
-	echo '<br>';
-
 	// Get order verification string
-	$o_id = '11';
 	$verification_string = "";
+
 	$result = mysqli_query($con, "SELECT verification_string FROM orders where customer_id ='$user_id' AND id = $o_id");
 
 	while($row = mysqli_fetch_array($result)) {
 		$verification_string = $row['verification_string'];
 	}
 
-	echo 'verification_string from order table: '.$verification_string;
-
-	echo '<br>';
-
-	$e_str = "";
-
-	$e_str = my_simple_crypt($verification_string, 'e', $pkey);
-
-	echo 'encrypted string: '.$e_str;
-
-	echo '<br>';
-
-
+	// Decrypted string
 	$decrypted_string = my_simple_crypt($e_str, 'd', $pkey);
 
-	echo 'decrypted string: '.$decrypted_string;
+	// Check for match between strings
+	if ($verification_string === $decrypted_string) {
 
-	if ($decrypted_string === $verification_string ) {
 		$verified = "Verified";
-
-		echo '<br>';
-		echo 'Updating status<br>';
 		$sql = "UPDATE orders SET status = '$verified' where id = '$user_id' AND id = '$o_id'";
 		$con->query($sql);
-		sleep(1);
-
-		//header("location:orders.php");
-	}
-	else {
+		header("location:orders.php");
+		
+	} else {
 		header("location:orders.php");
 	}
 
