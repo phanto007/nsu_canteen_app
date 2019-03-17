6e4b53757df2d0bd166cb3e6836b0ff430bcd138
phanto007 <adib.khan@northsouth.edu>
2019-03-17 12:07:03 +0600

    zxc

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
                 <div class="nav-wrapper">
                     <ul class="left">   
 
-                      <li><h1 class="logo-wrapper"> <a href="#" class="brand-logo darken-1">NSU Canteen</a> <span class="logo-text"></span></h1></li>
+                      <li><h1 class="logo-wrapper"> <a href="#" class="brand-logo darken-1">NSUa Canteen</a> <span class="logo-text"></span></h1></li>
                       <!--
                       <li><h1 class="logo-wrapper"><a href="#" class="brand-logo darken-1"><img src="images/materialize-logo.png" alt="logo"></a> <span class="logo-text">Logo</span></h1></li>
                     -->
