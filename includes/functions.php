3139dab163b9c5dacc70aaf097b3f323e6f40310
phanto007 <48092034+phanto007@users.noreply.github.com>
2019-03-30 19:41:39 +0600

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -51,4 +51,35 @@ function generateRandomString($length) {
     return $randomString;
 }
 
+function sendNotification($userid, $message){
+
+    $content = array(
+        "en" => $message
+    );
+    
+    $fields = array(
+        'app_id' => "d8bb0455-71e2-4ccd-af9a-ce48767d1709",
+        'filters' => array(array("field" => "tag", "key" => "userid", "relation" => "=", "value" => $userid)),
+        'data' => array("foo" => "bar"),
+        'contents' => $content
+    );
+    
+    $fields = json_encode($fields);
+
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
+                                               'Authorization: Basic OWU3MzEzMjEtNmU4ZS00MjAzLWFkNWItZDljMjBjMTliNTFh'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+    
+}
+
 ?>
