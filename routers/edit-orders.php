cd288bff4ae29d4ba916e735fc5a55d5f3b61800
phanto007 <48092034+phanto007@users.noreply.github.com>
2019-03-30 19:43:33 +0600

    Update edit-orders.php

--- routers/edit-orders.php
+++ routers/edit-orders.php
@@ -1,10 +1,17 @@
 <?php
 include '../includes/connect.php';
+include '../includes/functions.php';
 $status = $_POST['status'];
 $id = $_POST['id'];
 
 $sql = "UPDATE orders SET status='$status', status_delivered = '0',status_delivered_2 = '0' WHERE id=$id;";
 $con->query($sql);
+
+$result = mysqli_query($con, "SELECT customer_id FROM orders WHERE id = $id;");
+			while($row = mysqli_fetch_array($result))
+			{
+				sendNotification($row['customer_id'], "Order#".$id." Status Update: ".$status);
+			}
 
 header("location: ../all-orders.php");
 ?>
